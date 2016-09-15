ac27b8165ef8ae9a3f1917762355398cd68ffb19
Anshuman Prusty <anshuman.prusty@gmail.com>
2016-09-15 21:27:54 +0530

    hello

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,21 +26,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      redirect_to root_url, :notice => "Signed up!"
-    else
-      render "new"
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
-
-    #respond_to do |format|
-     # if @user.save
-      #  format.html { redirect_to @user, notice: 'User was successfully created.' }
-       # format.json { render :show, status: :created, location: @user }
-      #else
-       # format.html { render :new }
-        #format.json { render json: @user.errors, status: :unprocessable_entity }
-      #end
-    #end
   end
 
   # PATCH/PUT /users/1
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 
   def self.authenticate(email, password)
     user = find_by_email(email)
+    puts "**************" + user.to_s
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
       user
     else
