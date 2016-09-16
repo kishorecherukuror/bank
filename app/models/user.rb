745ffa02f8ab9bdd19252483d780e82452864b32
ASK <askgit1@gmail.com>
2016-09-16 15:05:10 +0530

    new 3:5pm

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,13 @@
 class User < ActiveRecord::Base
 	validates :fname, presence: true
 	validates :lname, presence: true
-	#validate :at_least_18
-	validates_inclusion_of :age, :in=>Date.new(1900)..Time.now.years_ago(18).to_date, :message=>'You must be 18 years or older'
-end
-
-
-def at_least_18
-
-    if self.age
-      errors.add(:age, 'You must be 18 years or older.') if self.age > 18.years.ago.to_date
-    end
+	
 	has_many :accounts
-
-	attr_accessor  :password, :password_confirmation
 	has_many :accounts
 	before_save :encrypt_password
+	attr_accessor  :password, :password_confirmation
+	validates_inclusion_of :age, :in=>Date.new(1900)..Time.now.years_ago(18).to_date, :message=>'You must be 18 years or older'
+	
 	def self.authenticate(email, password)
 	    user = find_by_email(email)
 	    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
@@ -31,5 +23,7 @@ def at_least_18
 	      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
 	    end
   	end
-
 end
+
+
+
